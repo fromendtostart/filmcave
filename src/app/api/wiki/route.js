f45844f50b40d156899f0566c3759a0e9df1642c
Chandramauli <chandramauliguptach@gmail.com>
2023-10-01 16:57:03 +0530

    set up route using wiki apin

---
+++ src/app/api/wiki/route.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export async function GET(request) {
+    // const film = await NextApiRequest.query["id"].toString();
+    console.log(request.nextUrl.searchParams.get(["key"]));
+    const response = await axios.get("https://en.wikipedia.org/w/api.php?action=query&prop=extracts&explaintext=true&exsectionformat=plain&titles=Physics");
+    return new Response(response.data, { status: 200 });
+}
