6f031e2b37fc89e4ecd9541b28f360041bdf5a8a
Chandramauli <chandramauliguptach@gmail.com>
2023-10-22 21:20:37 +0530

    integrated model wiki from backend

--- src/app/api/wiki/route.js
+++ src/app/api/wiki/route.js
@@ -10,18 +10,21 @@ const getExtract = (obj) => {
 export async function GET(request) {
     const film = request.nextUrl.searchParams.get(["id"]);
     const releaseYear = request.nextUrl.searchParams.get(["year"]);
-    let response = await axios.get(`https://en.wikipedia.org/w/api.php?action=query&prop=extracts&titles=${film}&format=json`);
+    const withReleaseYear = `https://en.wikipedia.org/w/api.php?action=query&prop=extracts&titles=${film}_(${releaseYear}_film)&format=json`;
+    const withFilm = `https://en.wikipedia.org/w/api.php?action=query&prop=extracts&titles=${film}_(film)&format=json`;
+    const justTitle = `https://en.wikipedia.org/w/api.php?action=query&prop=extracts&titles=${film}&format=json`;
+    let response = await axios.get(withReleaseYear);
     response = getExtract(response);
     if(response.length<4000)
     {
         console.log("previous not succesful");
-        response = await axios.get(`https://en.wikipedia.org/w/api.php?action=query&prop=extracts&titles=${film}_(film)&format=json`);
+        response = await axios.get(withFilm);
         response = getExtract(response);
     }
     if(response.length<4000)
     {
         console.log("previous not succesful");
-        response = await axios.get(`https://en.wikipedia.org/w/api.php?action=query&prop=extracts&titles=${film}_(${releaseYear}_film)&format=json`);
+        response = await axios.get(justTitle);
         response = getExtract(response);
     }
     if(response.length<4000)
@@ -29,6 +32,6 @@ export async function GET(request) {
         return new Response("Sorry! Can't find data, Try again later.", {status: 200})
     }
     response = response.slice(0, response.indexOf("<span id=\"References\">"));
-    console.log(response);
+    console.log(response.length);
     return new Response(JSON.stringify(response), { status: 200 });
 }
--- src/components/ChatUI.js
+++ src/components/ChatUI.js
@@ -2,6 +2,7 @@
 
 import { useState, useRef, useEffect } from "react";
 
+import "./ChatUI.css";
 import * as tf from "@tensorflow/tfjs";
 import * as qna from "@tensorflow-models/qna";
 
@@ -11,6 +12,7 @@ export default function ChatUI(props){
 
     const [answer, setAnswer] = useState("");
     const [model, setModel] = useState(null);
+    const [filmName, setFilmName] = useState("");
 
     const loadModel = async() => {
         const loadedModel = await qna.load();
@@ -22,6 +24,15 @@ export default function ChatUI(props){
         loadModel();
     }, []);
 
+    useEffect(()=>{
+        const stringBeforeTitle = `<p><i><b>`;
+        const stringAfterTitle = `</b></i>`;
+        setFilmName(props.filmWiki.slice(
+            props.filmWiki.indexOf(stringBeforeTitle)+9,
+            props.filmWiki.indexOf(stringAfterTitle)
+        ));
+    }, [props.filmWiki]);
+
     const reply = async (e) => {
         if(e.which === 13 && model!==null){
             const referenceText = props.filmWiki;
@@ -29,7 +40,6 @@ export default function ChatUI(props){
 
             const answers = await model.findAnswers(userQuery, referenceText);
             setAnswer(answers);
-            console.log(answers);
         }
         else if(model===null)
         {
@@ -45,7 +55,8 @@ export default function ChatUI(props){
                 </div>
                 :
                 <div className="ui">
-                    <input  ref={question} onKeyDown={reply}></input>
+                    <span className="filname">{filmName}</span>
+                    <input  ref={question} onKeyDown={reply} placeholder="Ask your question"></input>
                     {answer ? answer[0]:""}
                 </div>
             }
