8e902576c72f78dc6925bdb8ecbdddd0dd944179
Chandramauli <chandramauliguptach@gmail.com>
2023-10-23 23:06:38 +0530

    finished styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,11 +4,14 @@ export default function Navbar(){
     return(
         <div className="navbar">
             <div className="nav-headings">
-                <span className="logo">Logo</span>
                 <span className="title">Filmcave</span>
             </div>
             <div className="nav-items">
-                <span className="about">About</span>
+                <span className="about"><a href = "https://fromendtostart.github.io/MyPortfolio" style={{
+                                        textDecoration: "inherit",
+                                        color: "inherit",
+                                        }}
+                                        target="_blank"><button>My Portfolio</button></a></span>
             </div>
         </div>
     )
