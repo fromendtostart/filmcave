8ca922482bc38b1570eda4c9ea15ad7bbefe36fa
Chandramauli <chandramauliguptach@gmail.com>
2023-10-24 23:18:31 +0530

    added favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,6 +9,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body>
         <Navbar />
         {children}
